e79988d3663c89fe33282c3710a71505a1a01f5c
robbie <robbie@posiba.com>
2017-08-12 14:14:24 -0700

    update entry point for lecture 4

---
+++ src/data/configureStore.js
@@ -0,0 +1,24 @@
+/**
+ * Created by robbie on 8/12/17.
+ */
+import {loadState, saveState} from './localStorage';
+import throttle from 'lodash/throttle';
+import {createStore} from 'redux';
+import todoApp from '../reducers';
+// import registerServiceWorker from './registerServiceWorker';
+
+const configureStore = () => {
+  const persistedState = loadState();
+  let store = createStore(
+      todoApp,
+      persistedState
+  );
+  store.subscribe(throttle(() => {
+    saveState({
+      todos: store.getState().todos
+    });
+  }, 1000));
+  return store;
+};
+
+export default configureStore;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import todoApp from './reducers';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
-import App from './components/App';
-import {loadState, saveState} from './data/localStorage';
-import throttle from 'lodash/throttle';
-// import registerServiceWorker from './registerServiceWorker';
+import  configureStore from './data/configureStore';
+import Root from './components/Root';
 
-const persistedState = loadState();
+const store = configureStore();
 
-let store = createStore(
-    todoApp,
-    persistedState
-);
-
-store.subscribe(throttle(() => {
-  saveState({
-    todos: store.getState().todos
-  });
-}, 1000));
-
-console.log(store.getState());
 ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+    <Root store={store} />,
     document.getElementById('root')
 );
 // registerServiceWorker();
